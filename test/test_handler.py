5f18e294a2006ba9faaa47aba578f64bf52d2fa3
solid <karimbarbier00@gmail.com>
2021-05-05 18:02:57 +0200

    rename lambda_function.py to index.py

--- test/test_handler.py
+++ test/test_handler.py
@@ -1,5 +1,5 @@
 import unittest
-from src import index
+from src import Index
 
 
 class TestHandlerCase(unittest.TestCase):
@@ -7,13 +7,13 @@ class TestHandlerCase(unittest.TestCase):
     def test_response(self):
         print("testing response.")
         event={'Country':'USA'}
-        result = index.lambda_handler(event, None)
+        result = Index.lambda_handler(event, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
         self.assertIn('Hello from '+event['Country'], result['body'])
         event={'Country':'INDIA'}
-        result = index.lambda_handler(event, None)
+        result = Index.lambda_handler(event, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
